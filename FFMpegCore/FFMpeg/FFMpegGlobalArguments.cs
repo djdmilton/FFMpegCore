b41bf924b6fd5b2f62e4bc3f9ba1b82274dafce2
Daniel Milton <djdmilton@googlemail.com>
2024-01-14 10:01:01 +0000

    Added fluent global options for hiding the banner and for adding custom options.

--- FFMpegCore/FFMpeg/FFMpegGlobalArguments.cs
+++ FFMpegCore/FFMpeg/FFMpegGlobalArguments.cs
@@ -7,6 +7,8 @@ namespace FFMpegCore
         internal FFMpegGlobalArguments() { }
 
         public FFMpegGlobalArguments WithVerbosityLevel(VerbosityLevel verbosityLevel = VerbosityLevel.Error) => WithOption(new VerbosityLevelArgument(verbosityLevel));
+        public FFMpegGlobalArguments WithHideBanner() => WithOption(new HideBannerArgument());
+        public FFMpegGlobalArguments WithCustomOption(string argument) => WithOption(new CustomArgument(argument));
 
         // DJDM - made public so that global options may be extended outside of FFMpegCore
         public FFMpegGlobalArguments WithOption(IArgument argument)
